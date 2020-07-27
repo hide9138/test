e7b933f8985107ed6ba7336e15cb71e059c4b051
apple <apple@appurunoMacBook-Air.local>
2020-07-27 17:08:01 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'home#top'
-  devise_for :users
   get 'home/about' => 'home#about', as: 'about'
-  resources :users
+  resource :users
+  devise_for :users
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
